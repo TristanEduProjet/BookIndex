b3973acabfba1b56f78f491896941b3ecf6da800
TristanEduProjet <TristanEduProjet@users.noreply.github.com>
2017-12-19 10:44:11 +0100

    Add menu to MAinActivity

---
+++ app/src/main/java/fr/esgi/bookindex/ui/MainActivity.java
@@ -0,0 +1,100 @@
+package fr.esgi.bookindex.ui;
+
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import fr.esgi.bookindex.R;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.main_activity);
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        this.setSupportActionBar(toolbar);
+
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+
+        final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        final ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        ((NavigationView) findViewById(R.id.nav_view)).setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        this.getMenuInflater().inflate(R.menu.main_actionbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch(item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(final MenuItem item) {
+        // Handle navigation view item clicks here.
+        switch(item.getItemId()) {
+            case R.id.nav_search:
+                break;
+            case R.id.nav_bookloan:
+                break;
+            case R.id.nav_settings:
+                break;
+
+            case R.id.nav_save:
+                break;
+            case R.id.nav_export:
+                break;
+            case R.id.nav_send:
+                break;
+            case R.id.nav_share:
+                break;
+
+            case R.id.nav_about:
+                break;
+            case R.id.nav_help:
+                break;
+        }
+
+        ((DrawerLayout) findViewById(R.id.drawer_layout)).closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
