fec7850baaec1667c60df65784a053407659ceda
P.-H. Langlois <224817@supinfo.com>
2018-02-01 23:49:35 +0100

    Add Book entity and DAO

---
+++ app/src/main/java/fr/esgi/bookindex/entities/Book.java
@@ -0,0 +1,56 @@
+package fr.esgi.bookindex.entities;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+
+@Entity(tableName = "books")
+public class Book {
+
+    @PrimaryKey
+    private int id;
+
+    @ColumnInfo
+    private String name;
+
+    @ColumnInfo
+    private String author;
+
+    @ColumnInfo
+    private String description;
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
